baa087e800f8e6c9fda491902ca65db69116e8f6
sergiferre <sergiferre@mbp-de-sergi.home>
2020-12-29 13:43:50 +0100

    Initial commit. Spring boot project.

---
+++ albi/src/main/java/com/albi/AlbiApplication.java
@@ -0,0 +1,13 @@
+package com.albi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbiApplication.class, args);
+	}
+
+}
---
+++ albi/src/test/java/com/albi/AlbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
